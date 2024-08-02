3b4c1a2b2ab87fc16b54825e00b49cae952a69bf
Gustavo Landtreter <gslandtreter@gmail.com>
2024-08-02 17:44:23 -0300

    Enabled risc0 dev mode, updated ports

--- init/init.sh
+++ init/init.sh
@@ -5,7 +5,7 @@ set -e
 
 while :
 do
-  IS_READY=$(curl -sLX POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":"id","method":"all_nodes_ready","params":[]}' http://bootnode:9000/ | jq .result)
+  IS_READY=$(curl -sLX POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":"id","method":"all_nodes_ready","params":[]}' http://bootnode:9001/ | jq .result)
   if [ "$IS_READY" = true ] ; then
     break;
   fi
@@ -18,13 +18,13 @@ echo "Nodes are ready! Running start_key_exchange"
 curl -sLX POST \
   -H 'Content-Type: application/json' \
   -d '{"jsonrpc":"2.0","id":"id","method":"start_key_exchange","params":[]}' \
-  http://bootnode:9000/
+  http://bootnode:9001/
 
 echo -e "\nRunning start_dkg"
 
 curl -sLX POST \
   -H 'Content-Type: application/json' \
   -d '{"jsonrpc":"2.0","id":"id","method":"start_dkg","params":[]}' \
-  http://bootnode:9000/
+  http://bootnode:9001/
 
 echo -e "\nDone!"
